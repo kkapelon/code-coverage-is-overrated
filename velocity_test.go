ba3b2f26eaaae4ed7b0c22b027638c34b0fcf677
Kostis Kapelonis <kkapelon@users.noreply.github.com>
2024-06-23 16:56:32 +0000

    Finished tests

--- velocity_test.go
+++ velocity_test.go
@@ -1,16 +1,30 @@
 package velocity
 
 import (
+	"fmt"
 	"testing"
 )
 
-// TestHelloName calls greetings.Hello with a name, checking
-// for a valid return value.
-func TestHelloName(t *testing.T) {
-	angle := 5
-	direction := 3
-	velocity := calculateVelocity(angle, direction)
-	if velocity != 4 {
-		t.Fatalf(`Wanted %d, got %d`, 4, velocity)
+func TestVelocityScenarios(t *testing.T) {
+
+	parameters := []struct{ exampleAngle, exampleDirection, expectedVelocity int }{
+		{3, 4, 5},
+		{10, 2, 2},
+		{8, 3, 2},
+		{6, 6, 3},
+		{12, 7, 3},
+		{22, 8, 2},
+		{1, 20, 1},
 	}
+
+	for i := range parameters {
+		t.Run(fmt.Sprintf("Testing [%v]", i), func(t *testing.T) {
+			actual := calculateVelocity(parameters[i].exampleAngle, parameters[i].exampleDirection)
+			if actual != parameters[i].expectedVelocity {
+				t.Logf("expected%d: , actual:%d", parameters[i].expectedVelocity, actual)
+				t.Fail()
+			}
+		})
+	}
+
 }
